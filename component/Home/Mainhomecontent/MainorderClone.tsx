d32ee3167b643c099ffbca9f57cf671a95df6224
while <while.gn@gmail.com>
2023-05-19 12:33:12 +0330

    update

--- component/Home/Mainhomecontent/MainorderClone.tsx
+++ component/Home/Mainhomecontent/MainorderClone.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./MainorderClone.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const MainorderClone: React.FC = function (props) {
+  const router = useRouter();
+
+  const MainorderHandler = function (e: any) {
+    router.push("/");
+  };
+
   return (
     <>
-      <div className="Main_MainorderClone">
-        <div className="MainorderClone"></div>
-        {/* <Link href={""} className="orderLinkSelf"></Link> */}
+      <div onClick={MainorderHandler} className="Main_MainorderClone">
+        <div className="MainorderClone"> tea adn cooffe</div>
       </div>
     </>
   );
