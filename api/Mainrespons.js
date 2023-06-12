e6c312ce1ee048910d5e758b973a51a63761bae4
while <while.gn@gmail.com>
2023-06-12 18:14:45 +0330

    update

--- api/Mainrespons.js
+++ api/Mainrespons.js
@@ -3,16 +3,21 @@
 // import ApiHandler from "./Mainfechdata";
 async function Mainfechdata() {
   let jsonData;
-  console.log("ooooooooooooooooooooooooooooooooooooo");
-  const SendResponse = await fetch(
-    "https://maincoffe-a99a3-default-rtdb.firebaseio.com/.json"
-  );
-  jsonData = await SendResponse.json();
-  console.log(jsonData);
+
+  // const SendResponse = await fetch(
+  //   "https://jsonplaceholder.typicode.com/todos/1"
+  // );
+
+  // jsonData = await SendResponse.json();
+  // console.log(jsonData);
   try {
+    // const SendResponse = await fetch(
+    //   "https://maincoffe-a99a3-default-rtdb.firebaseio.com/.json"
+    // );
     const SendResponse = await fetch(
       "https://maincoffe-a99a3-default-rtdb.firebaseio.com/.json"
     );
+
     jsonData = await SendResponse.json();
     console.log(jsonData);
     return jsonData;
@@ -22,3 +27,5 @@ async function Mainfechdata() {
 }
 
 export default Mainfechdata;
+
+// "https://jsonplaceholder.typicode.com/todos/1"
