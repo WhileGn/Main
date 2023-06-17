c0134fae83757233380d4fa5c4dcaed38c9f3b21
while <while.gn@gmail.com>
2023-06-17 19:19:48 +0330

    update

--- component/about_coffe/Mainabout.tsx
+++ component/about_coffe/Mainabout.tsx
@@ -7,20 +7,25 @@ import { motion } from "framer-motion";
 
 export function Mainabout() {
   return (
-    <div className="Main_about">
-      <div className="Main_about_child_1">
-        <div className="about">
-          <NavbarAboutCoffe></NavbarAboutCoffe>
-          <Maincontent></Maincontent>
+    <>
+      <div className="Main_about">
+        <div className="abouts">
+          <div className="Main_about_child_1">
+            <div className="about">
+              <NavbarAboutCoffe></NavbarAboutCoffe>
+              <Maincontent></Maincontent>
+            </div>
+          </div>
+          {/* <div className="Main_about_child_borderbetincontent"></div> */}
+          <div className="Main_about_child_2">
+            <ChildContent></ChildContent>
+          </div>
         </div>
-      </div>
-      {/* <div className="Main_about_child_borderbetincontent"></div> */}
-      <div className="Main_about_child_2">
-        <div className="about">
-          <ChildContent></ChildContent>
+
+        <div className="FoterAbout">
           <Footer></Footer>
         </div>
       </div>
-    </div>
+    </>
   );
 }
