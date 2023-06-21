90197e385c63ce5fc2cf109b58a341da93102ced
while <while.gn@gmail.com>
2023-06-21 15:02:24 +0330

    update

--- component/about_coffe/navbarAboutCoffe/NavbarAboutCoffe.tsx
+++ component/about_coffe/navbarAboutCoffe/NavbarAboutCoffe.tsx
@@ -1,18 +1,27 @@
 import "./NavbarAboutCoffe.css";
-import React from "react";
+import React, { useRef } from "react";
 // import { Link, NavLink } from "react-router-dom";
 import Link from "next/link";
 import { useState } from "react";
 import { GoListUnordered } from "react-icons/go";
+import { useIntersectionObserver } from "usehooks-ts";
 
 const NavbarAboutCoffe: React.FC = function (props) {
   // const [mainstate, setmainstate] = useState(0);
+
+  const ref_0: any = useRef().current;
+  const ref_1: any = useRef().current;
+  const entry_1 = useIntersectionObserver(ref_1, {});
+
   return (
     <div className="Snavbar_Main">
       <div className="Snavbar_icon">
         <p className="Snavbar_icon_self">Coffee</p>
       </div>
-      <div className="Snavbar_btn_Maindiv">
+      <div ref={ref_0} id="navbar_mobileresponsve_icon" className="hiden">
+        <GoListUnordered></GoListUnordered>
+      </div>
+      <div ref={ref_1} className="Snavbar_btn_Maindiv">
         <Link className="Snavbar_btn" href={"/"}>
           Home
         </Link>
